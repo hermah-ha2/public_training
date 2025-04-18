04fcc58a62a22cc8f064a02daf20e15a43734272
Herman Hardersen <hermahar@uio.no>
2025-04-18 14:21:54 +0200

    la till festival team fil og prøvde å legge til et bilde og en component som hadde bilde med text

---
+++ trainingwheels/src/app/components/Festivalvteam.tsx
@@ -0,0 +1,18 @@
+import bilde1 from "./textbilder/bilde1";
+
+export default function Festivalteam(){
+    return(
+        <section className="">
+            <div className="py-5"></div>
+            <div className="border-[1px] border-black mx-20 "> 
+            </div>
+            <h2 className="text-black text-5xl font-title px-20 my-1 " > Festivalteam</h2>
+            <div className="border-1 border-black mx-20 ">
+            <bilde1></bilde1> 
+            </div>
+            
+        </section>
+        
+    )
+
+}
---
+++ trainingwheels/src/app/components/textbilder/bilde1.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Image from "next/image"
+
+export default function bilde1(){
+
+    return(
+        <section className="">
+            
+            <Image src={"SiriF.png"} alt="bilde" width={1065} height={730} className="flex"/> 
+        </section>
+        
+    )
+
+}
